de30e63c86ec2a3332c7845fb3b1740fd4015e60
hawar <iaramkhzri@gmail.com>
2025-07-29 18:32:43 +0330

    Porject init

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center">
+
+# 📗 Study Plus, A tool for easier study.
+
+This will make your life easier with providing a dashboard for your study workflow ⏫
+</div>
+
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-3xl font-funnel bg-green-950 text-green-200 flex  h-dvh justify-center items-center">
+      Study Plus
+    </div>
+  );
+}
