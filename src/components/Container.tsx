a98a89b3d094eb0fba873d08e8b88697ceacc1db
hawar <iaramkhzri@gmail.com>
2025-08-07 23:28:46 +0330

    [+] added shadow to container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 };
 export default function Container({ title, children }: ContainerProps) {
   return (
-    <div className="bg-gray-100 rounded-2xl min-w-[min(60vw,20rem)]">
+    <div className="bg-gray-100 rounded-2xl min-w-[min(60vw,20rem)] shadow-xl">
       <div className="bg-green-100 rounded-2xl">
         <h1 className="text-primary-600 font-bold text-[1.2rem] md:text-[1.6rem] px-4 py-4 text-center">
           {title}
