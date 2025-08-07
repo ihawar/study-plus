9d85ffbecf6814608396d0acd57e3612c13ddfa2
hawar <iaramkhzri@gmail.com>
2025-08-07 22:40:29 +0330

    [+] container component added

---
+++ src/components/Container.tsx
@@ -0,0 +1,16 @@
+type ContainerProps = {
+  title: string;
+  children: React.ReactNode;
+};
+export default function Container({ title, children }: ContainerProps) {
+  return (
+    <div className="bg-gray-100 rounded-2xl min-w-[min(60vw,20rem)]">
+      <div className="bg-green-100 rounded-2xl">
+        <h1 className="text-primary-600 font-bold text-[1.2rem] md:text-[1.6rem] px-4 py-4 text-center">
+          {title}
+        </h1>
+      </div>
+      <div className="px-4 py-4">{children}</div>
+    </div>
+  );
+}
