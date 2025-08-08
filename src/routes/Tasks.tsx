d010a5214d3268a2919e3f8aef8ddef982ef0673
hawar <iaramkhzri@gmail.com>
2025-08-08 17:05:09 +0330

    [+] Tasks route updated

--- src/routes/Tasks.tsx
+++ src/routes/Tasks.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import CreateTopic from "@/components/CreateTopic";
+import AddTask from "@/components/tasks/AddTask";
+import ManageTasks from "@/components/tasks/ManageTasks";
 
 export default function Tasks() {
   return (
-    <div>Tasks</div>
-  )
+    <div className="flex flex-col-reverse md:flex-col gap-20">
+      <div className="flex flex-col md:flex-row gap-5">
+        <AddTask />
+        <CreateTopic />
+      </div>
+      <ManageTasks />
+    </div>
+  );
 }
