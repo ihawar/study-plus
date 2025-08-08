0f1cdfea0eca1459cf8d512cead608326a7add6c
hawar <iaramkhzri@gmail.com>
2025-08-08 17:04:49 +0330

    [*] container bug fix

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,13 +4,15 @@ type ContainerProps = {
 };
 export default function Container({ title, children }: ContainerProps) {
   return (
-    <div className="bg-gray-100 rounded-2xl min-w-[min(60vw,20rem)] shadow-xl">
+    <div className="bg-gray-100 rounded-2xl min-w-[min(60vw,20rem)] shadow-xl h-fit">
       <div className="bg-green-100 rounded-2xl">
         <h1 className="text-primary-600 font-bold text-[1.2rem] md:text-[1.6rem] px-4 py-4 text-center">
           {title}
         </h1>
       </div>
-      <div className="px-4 py-4">{children}</div>
+      <div className="px-4 py-4 flex justify-center items-center">
+        {children}
+      </div>
     </div>
   );
 }
